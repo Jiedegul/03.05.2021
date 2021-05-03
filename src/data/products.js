68f4899c40e0babd1c53aeaa8d4094729f692635
Elisei <=>
2021-05-03 21:01:24 +0600

    first commit

---
+++ src/data/products.js
@@ -0,0 +1,39 @@
+
+
+
+const products =[
+    {
+        id:1,
+        title:"MI",
+        price:120,
+        image:"http://www.xiaomi.kg/wp-content/uploads/2020/11/pms_1606304737.20344285-1200x1200.png",
+        description:""
+        
+    },
+    {
+        id:2,
+        title:"Redmi",
+        price:100,
+        image:"https://www.ixbt.com/img/n1/news/2021/3/2/photo_2021-04-13_10-56-49_large.jpg",
+        description:""
+        
+    },
+    {
+        id:3,
+        title:"samsung",
+        price:80,
+        image:"https://samsungkg.kg/cache/files/8901.jpg_w630_h480_resize.jpg?t=1616427906",
+        description:""
+        
+    },
+    {
+        id:4,
+        title:"iphone",
+        price:60,
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzvpkUSe3rNbILmQVQyujzyp0z69TB535k4A&usqp=CAU",
+        description:""
+        
+    }
+]
+
+export default products;
---
+++ src/reduce/index.js
@@ -0,0 +1,8 @@
+import products from "../data/products"
+
+
+const reduce =( state=products,action)=>{
+    return state
+}
+
+export default reduce;
